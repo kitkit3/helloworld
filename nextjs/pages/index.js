b7dd44356da8976d6c369d132d62672852e402c9
kitkit3 <garylai4@gmail.com>
2023-01-12 22:31:01 +0800

    2022/1/13 nextjs3

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+//import styles from '../styles/Home.module.css'
 import React, { useState,createContext,useContext,Fragment,useCallback, useMemo } from 'react';
 import './index.css'
 const inter = Inter({ subsets: ['latin'] })
