fdce791345d235029978da1c68a7174dd9637929
kitkit3 <garylai4@gmail.com>
2023-01-18 20:41:36 +0800

    2023/1/18nextjs2

--- nextjs/pages/index(old).js
+++ nextjs/pages/index(old).js
@@ -1,21 +1,29 @@
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
+import { END } from 'redux-saga'
 import { wrapper } from '../store'
-import { startClock, tickClock } from '../actions'
+import { loadData, startClock, tickClock } from '../actions'
 import Page from '../components/page'
 
-const Other = () => {
+const Index = () => {
   const dispatch = useDispatch()
 
   useEffect(() => {
     dispatch(startClock())
   }, [dispatch])
 
-  return <Page title="Other Page" linkTo="/" NavigateTo="Index Page" />
+  return <Page title="Index Page" linkTo="/other" NavigateTo="Other Page" />
 }
 
 export const getStaticProps = wrapper.getStaticProps(async ({ store }) => {
   store.dispatch(tickClock(false))
+
+  if (!store.getState().placeholderData) {
+    store.dispatch(loadData())
+    store.dispatch(END)
+  }
+
+  await store.sagaTask.toPromise()
 })
 
-export default Other
+export default Index
--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,29 +1,21 @@
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
-import { END } from 'redux-saga'
 import { wrapper } from '../store'
-import { loadData, startClock, tickClock } from '../actions'
+import { startClock, tickClock } from '../actions'
 import Page from '../components/page'
 
-const Index = () => {
+const Other = () => {
   const dispatch = useDispatch()
 
   useEffect(() => {
     dispatch(startClock())
   }, [dispatch])
 
-  return <Page title="Index Page" linkTo="/other" NavigateTo="Other Page" />
+  return <Page title="Other Page" linkTo="/" NavigateTo="Index Page" />
 }
 
 export const getStaticProps = wrapper.getStaticProps(async ({ store }) => {
   store.dispatch(tickClock(false))
-
-  if (!store.getState().placeholderData) {
-    store.dispatch(loadData())
-    store.dispatch(END)
-  }
-
-  await store.sagaTask.toPromise()
 })
 
-export default Index
+export default Other
